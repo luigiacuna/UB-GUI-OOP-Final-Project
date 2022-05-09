b68b6d0187da32520bb959339a1ee50214f5af3f
luigiacuna <100301719+luigiacuna@users.noreply.github.com>
2022-05-09 12:08:44 -0600

    mod the delclareation for addUser
    removed the int parameter since the functions handles it on its own and readded combobox currenttext

--- database.h
+++ database.h
@@ -11,7 +11,7 @@ class Database
 public:
     Database();
     bool userNameAvaliabity(QString);//used in the NewUser Class and login class
-    bool addUser(int,QString,QString,QString,QString,QString,int);//NewUser Class
+    bool addUser(QString,QString,QString,QString,QString,int);//NewUser Class
     bool checkCredentials(QString, QString);//used in Login Class
     QString getRole(QString);//used in Login Class
     QSqlQueryModel* adminTable(int); //admin window to show username
--- newuser.cpp
+++ newuser.cpp
@@ -143,7 +143,7 @@ void NewUser::onOk()
         if(okFirstName==true&&okLastName==true&&okUsername==true&&okPassword==true)
         {
             credsCheck=true;
-            //db.addUser(0,ui->userNameInput->text(),ui->firstNameInput->text(),ui->lastNameInput->text(),ui->passwordInput->text(),"",1);
+            db.addUser(ui->userNameInput->text(),ui->firstNameInput->text(),ui->lastNameInput->text(),ui->passwordInput->text(),ui->roleComboBox->currentText(),1);
             //this->close();
             //Login log;
             //log.setModal(true);
